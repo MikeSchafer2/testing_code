b547a1975ab8367b87303fd6081f6cef15d50741
himanshu.pandey <himanshu.pandey@magicedtech.com>
2024-02-21 14:37:09 +0530

    changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)jj
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
